3a471c13abc80a3ae82fb07444c1e3f220db1b56
shjung <shjung@urielsoft.co.kr>
2024-10-18 11:22:17 +0900

    feat : 승객 항공편 예약 UI 생성

---
+++ src/main/java/uriel/uam/controller/ui/UamController.java
@@ -0,0 +1,20 @@
+package uriel.uam.controller.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UamController {
+
+    @GetMapping
+    public String index(){
+        return "html/index.html";
+    }
+
+    @GetMapping("createReservation")
+    public String createReservation(){
+        return "html/createReservation.html";
+    }
+}
